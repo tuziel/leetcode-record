ef0fc1f1003c7be45ad91b49ce38916d78207c7e
tuziel <tuziel@outlook.com>
2020-05-04 14:22:57 +0800

    update: 更新为 O(n) / O(1) 的解法

--- src/LC45-jump-game-ii/index.js
+++ src/LC45-jump-game-ii/index.js
@@ -3,26 +3,34 @@
  * @return {number}
  */
 var jump = function (nums) {
+  var max = (a, b) => a > b ? a : b;
   var end = nums.length - 1;
-  if (end <= 0) return 0;
+  var res = 0;
+  var index = 0;
+  var right = 0;
+  var next = 0;
 
-  var min = (a, b) => a < b ? a : b;
-  var dp = new Array(nums.length);
-  var i, j, m;
-  dp[i = end] = 0;
+  /**
+   * 可将 nums 按能跳到最远的地方，划分为多个区间
+   * 初始区间为 [nums[0]]
+   * [4] 3 2 4 1] 0 4 2]
+   * 显然，从一个区间跳到下一个区间至少需要跳一次
+   */
 
-  while (i--) {
-    j = i + nums[i];
-    if (j < end) {
-      m = Infinity;
-      while (j > i) m = min(m, dp[j--] + 1);
-      dp[i] = m;
-    } else {
-      dp[i] = 1;
+  // 当前区间能够一步跳到终点则结束循环
+  while (right < end) {
+    // 当前区间能跳到的最远值
+    next = max(next, index + nums[index]);
+    // 到达当前区间结尾，设置下个区间范围
+    if (index++ === right) {
+      // 判断是否能跳到下一个区间，但是这题没有无法到达终点的样例
+      // if (next === right) return -1;
+      next = right = next;
+      res++;
     }
   }
 
-  return dp[0];
+  return res;
 };
 
 module.exports = jump;
