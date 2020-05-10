2260f3cf3b630dea3d5084528654ba4af4cc829d
tuziel <tuziel@outlook.com>
2020-05-11 00:55:48 +0800

    update: 删除无用变量

--- src/LC5405-count-triplets-that-can-form-two-arrays-of-equal-xor/index.js
+++ src/LC5405-count-triplets-that-can-form-two-arrays-of-equal-xor/index.js
@@ -6,26 +6,26 @@ var countTriplets = function (arr) {
   var len = arr.length;
   if (len < 2) return 0;
 
-  var i, j, k, l1, l2, l3, r1, r2, r3;
-  i = j = k = l1 = l2 = l3 = r1 = r2 = r3 = 0;
+  var i, j, k, l1, l2, r1, r2;
+  i = j = k = l1 = l2 = r1 = r2 = 0;
   var total = 0;
 
   while (i < len - 1) {
     k = i + 1;
-    l2 = l1 = arr[i];
-    r2 = r1 = arr[k];
+    l1 = arr[i];
+    r1 = arr[k];
     while (k < len) {
       j = i + 1;
-      l3 = l2;
-      r3 = r2;
+      l2 = l1;
+      r2 = r1;
       while (j <= k) {
-        if (l3 === r3) total++;
+        if (l2 === r2) total++;
         j++;
-        l3 ^= arr[j];
-        r3 ^= arr[j];
+        l2 ^= arr[j];
+        r2 ^= arr[j];
       }
       k++;
-      r2 ^= arr[k];
+      r1 ^= arr[k];
     }
     i++;
     l1 ^= arr[i];
