224bcd71b70f31338a0a541c234279e1bc5d2875
tuziel <tuziel@outlook.com>
2020-10-20 04:34:19 +0800

    update: 添加 jsdoc 及字符串切片方法

--- src/utils.js
+++ src/utils.js
@@ -4,17 +4,36 @@ module.exports = {
   createTreeList,
   ListNode,
   createList,
+  slice64,
 };
 
+/**
+ * 生成树节点
+ * @class
+ * @template T
+ * @param {T} val 节点的值
+ */
 function TreeNode(val) {
   this.val = val;
   this.left = this.right = null;
 }
 
+/**
+ * 生成树并返回根节点
+ * @template T
+ * @param {T[]} values 节点值的列表
+ * @returns {TreeNode<T>} 树的根节点
+ */
 function createTree(values) {
   return createTreeList(values)[0] || null;
 }
 
+/**
+ * 生成树并返回节点列表
+ * @template T
+ * @param {T[]} values 节点值的列表
+ * @returns {TreeNode<T>[]} 树的节点列表
+ */
 function createTreeList(values) {
   const len = values.length;
   if (!len) return [];
@@ -38,11 +57,23 @@ function createTreeList(values) {
   return nodes;
 }
 
+/**
+ * 生成链表节点
+ * @class
+ * @template T
+ * @param {T} val 节点的值
+ */
 function ListNode(val) {
   this.val = val;
   this.next = null;
 }
 
+/**
+ * 生成链表并返回头节点
+ * @template T
+ * @param {T[]} values 节点值的列表
+ * @returns {ListNode<T>[]} 链表的头节点
+ */
 function createList(values) {
   let len = values.length;
   let node = null;
@@ -53,4 +84,11 @@ function createList(values) {
     node = prev;
   }
   return node;
+}
+
+/**
+ * 转为字符串并限制为64字符内
+ */
+function slice64(data) {
+  return ((i) => `${i.length > 64 ? `${i.slice(0, 61)}...` : i}`)(JSON.stringify(data));
 }
