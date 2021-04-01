d0197f400b08e23bb66e789fb76d08abdf15e832
tuziel <tuziel@outlook.com>
2021-04-02 01:10:19 +0800

    formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   'env': {
     'es2020': true,
-    "commonjs": true,
-    'node': true
+    'commonjs': true,
+    'node': true,
   },
   'extends': 'eslint:recommended',
   'globals': {
@@ -14,33 +14,33 @@ module.exports = {
   },
   'parserOptions': {
     'ecmaVersion': 11,
-    'sourceType': 'module'
+    'sourceType': 'module',
   },
   'rules': {
     'comma-dangle': [
-      "error",
-      "always-multiline",
+      'error',
+      'always-multiline',
     ],
     'indent': [
       'error',
       2,
-      { "SwitchCase": 1 }
+      { 'SwitchCase': 1 },
     ],
     'quotes': [
       'error',
-      'single'
+      'single',
     ],
     'semi': [
       'error',
-      'always'
+      'always',
     ],
-    "eqeqeq": [
-      "error",
-      "always",
-      { "null": "ignore" }
+    'eqeqeq': [
+      'error',
+      'always',
+      { 'null': 'ignore' },
     ],
-    "no-constant-condition": "off",
-    "no-console": "warn",
-    "no-unused-vars": "warn"
-  }
+    'no-constant-condition': 'off',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+  },
 };
