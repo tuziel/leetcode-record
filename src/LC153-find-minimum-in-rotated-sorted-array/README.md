ea33137742ea3be97245881e50c0da60cf56a90b
tuziel <tuziel@outlook.com>
2021-04-08 23:59:22 +0800

    update: 添加题目及题解
    * 153. 寻找旋转排序数组中的最小值

---
+++ src/LC153-find-minimum-in-rotated-sorted-array/README.md
@@ -0,0 +1,9 @@
+---
+tags:
+- 数组
+- 二分查找
+---
+
+# 153. 寻找旋转排序数组中的最小值 [Medium]
+
+<https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/>
---
+++ src/LC153-find-minimum-in-rotated-sorted-array/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  var left = 0;
+  var right = nums.length - 1;
+  var min = nums[0];
+
+  while (left <= right) {
+    var mid = (left + right) >> 1;
+
+    if (nums[mid] < min) {
+      right = mid - 1;
+      min = nums[mid];
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return min;
+};
+
+module.exports = findMin;
